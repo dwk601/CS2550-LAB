4ba0f3870d749ed50c5419d8e52bac34bab4a63a
dwk601 <10829539@uvu.edu>
2022-10-09 20:55:14 -0600

    cs2550 4.12 date object

---
+++ stringObject.js
@@ -0,0 +1,24 @@
+// let wiseSaying = "A picture is worth a thousand words."; // Code will be tested with "You can lead a horse to water, but you can't make him drink. If you want something done right, you have to do it yourself."
+// let timesAppeared = 0;
+
+// //Loop through the characters in the string wiseSaying and assign timesAppeared with the number of times 'o' appears in the string.
+
+// for (let i = 0; i < wiseSaying.length; i++) {
+//   if (wiseSaying[i] === 'o') {
+//     timesAppeared++;
+//   }
+// }
+
+// let wiseProverb = "Actions shout louder than words."; // Code will be tested with "You don't have to shout so loud."
+// let substringIndex = 0;
+
+// //Find the index of the string "shout" and assign substringIndex with the index. Then, update wiseProverb replacing "shout" with "speak".
+
+// substringIndex = wiseProverb.indexOf('shout');
+// wiseProverb = wiseProverb.replace('shout', 'speak');
+
+// let oldSaying = "When in Rome do as Romans do."; // Code will be tested with "The Fontana di Trevi, in Rome, was my favorite."
+
+// //Use indexOf(), and substr() or substring(), to display the end of the sentence starting from the word "Rome".
+
+// console.log(oldSaying.substring(oldSaying.indexOf('Rome')));
